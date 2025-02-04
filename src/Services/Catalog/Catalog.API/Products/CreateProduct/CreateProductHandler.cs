5c3f828c2b280afd561082240d568fab504ff832
russelake@gmail.com <russelake@gmail.com>
2025-02-04 20:01:29 +0800

    Add MediaTR package

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string description, string ImageFile, decimal Price)
+    :IRequest<CreatreProductResult>;
+    public record CreatreProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreatreProductResult>
+    {
+        public Task<CreatreProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // Business logic to create a product
+
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container.
+
 var app = builder.Build();
 
+
+// Create endpoint for hello world
 app.MapGet("/", () => "Hello World!");
+
+
+// Configure the HTTP request pipeline.
 
 app.Run();
