3da89241484a0a160fcd038842ba4abd34e26d3b
russelake@gmail.com <russelake@gmail.com>
2025-02-05 14:45:03 +0800

    Add Product Minimal API

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,18 +1,35 @@
-﻿using MediatR;
+﻿
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string description, string ImageFile, decimal Price)
-    :IRequest<CreatreProductResult>;
-    public record CreatreProductResult(Guid Id);
+    : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreatreProductResult>
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreatreProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            // Business logic to create a product
+            // Create Product Entity From Command Object
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
 
-            throw new NotImplementedException();
+
+            // Save To Database
+
+
+
+            // Return CreatreProductResult Result
+            return new CreateProductResult(Guid.NewGuid());
         }
     }
 }
