e4b511765bd1063207c89df060b88afaf51d1960
russelake@gmail.com <russelake@gmail.com>
2025-02-05 15:34:40 +0800

    Add custom configuration in Carter package

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,14 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
-//Add services to the container.
+//Add services to the container. // add the carter service
+builder.Services.AddCarter(configurator: c =>
+{
+    // Specify the assembly containing your modules
+    var modulesAssembly = typeof(Program).Assembly;
+    var modules = modulesAssembly.GetTypes()
+        .Where(t => typeof(ICarterModule).IsAssignableFrom(t) && !t.IsAbstract)
+        .ToArray();
+    c.WithModules(modules);
+});
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 
 var app = builder.Build();
 
 
-// Create endpoint for hello world
-app.MapGet("/", () => "Hello World!");
-
-
 // Configure the HTTP request pipeline.
+app.MapCarter(); // Scan all the ICarterModule in the project and map the necessary route
 
 app.Run();
