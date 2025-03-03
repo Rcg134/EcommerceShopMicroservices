b9484e8f6b0b464f055692d944091cb606b9a5b3
russelake@gmail.com <russelake@gmail.com>
2025-03-03 16:54:05 +0800

    Add basket IRepository contract

---
+++ src/Services/Basket/Basket_API/Data/BasketRepository.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Basket_API.Data;
+
+public class BasketRepository
+    (IDocumentSession session) 
+    : IBasketRepository
+{
+    public Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
